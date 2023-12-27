e56a6db844ebd3b3de8650da690495d4f5a7bc4a
Darwin Dingel <darwin@cometbackup.com>
2023-12-27 17:22:13 +1300

    Update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,7 +22,10 @@ require("lazy").setup({
 		build = { ':TSUpdate' }
 	},
 	{
-		'nvim-lualine/lualine.nvim'
+		'nvim-lualine/lualine.nvim',
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		}
 	},
 	{
 		"nvim-neo-tree/neo-tree.nvim",
@@ -38,17 +41,70 @@ require("lazy").setup({
 		'neovim/nvim-lspconfig',
 	},
 	{
-		'neoclide/coc.nvim',
-		branch = 'release',
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+			'neovim/nvim-lspconfig',
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-buffer',
+			'hrsh7th/cmp-path',
+			'hrsh7th/cmp-cmdline',
+		}
+	},
+	{
+		'L3MON4D3/LuaSnip',
+		dependencies = {
+			'saadparwaiz1/cmp_luasnip',
+		},
+	},
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			'nvim-neotest/neotest-go',
+		}
+	},
+	{
+		"tpope/vim-fugitive"
+	}
+})
+
+-- neotest
+--[[
+require('neotest').setup({
+	adapters = {
+		require("neotest-go")({
+		}),
+		require("neotest-plenary"),
+		require("neotest-vim-test")({
+			ignore_file_types = { "go", "vim", "lua" },
+		})
+	},
+	keys = {
+		{ "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
+		{ "<leader>tT", function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Run All Test Files" },
+		{ "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
+		{ "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
+		{ "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
+		{ "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
+		{ "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop" },
 	},
 })
+--]]
 
 -- Telescope
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>gi', builtin.lsp_incoming_calls, {})
+vim.keymap.set('n', '<leader>go', builtin.lsp_outgoing_calls, {})
+vim.keymap.set('n', '<leader>gr', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>gI', builtin.lsp_implementations, {})
+vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {})
 
 require('telescope').setup{
 	defaults = {
@@ -64,31 +120,35 @@ require('neo-tree').setup({
 })
 vim.keymap.set('n', '<C-d>', '<Cmd>Neotree toggle<CR>', {silent=true})
 
--- COC
-vim.keymap.set('n', 'gd', '<Plug>(coc-definition)', {silent=true})
-vim.keymap.set('n', 'gy', '<Plug>(coc-type-definition)', {silent=true})
-vim.keymap.set('n', 'gr', '<Plug>(coc-references)', {silent=true})
-vim.keymap.set('n', 'K', 'CocActionAsync("doHover")', {silent=true})
-vim.keymap.set('n', '<C-space>', 'coc#refresh()', {silent=true})
---
--- Use <c-j> to trigger snippets
-local keyset = vim.keymap.set
-keyset("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)")
--- Use <c-space> to trigger completion
-keyset("i", "<c-space>", "coc#refresh()", {silent = true, expr = true})
+-- nvim-cmp
+local cmp = require('cmp')
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = 'path' },
+		{ name = 'nvim_lsp', keyword_length = 1 },
+		{ name = 'buffer', keyword_length = 3 },
+		{ name = 'luasnip', keyword_length = 2 },
+	})
+})
 
--- Use K to show documentation in preview window
-function _G.show_docs()
-    local cw = vim.fn.expand('<cword>')
-    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
-        vim.api.nvim_command('h ' .. cw)
-    elseif vim.api.nvim_eval('coc#rpc#ready()') then
-        vim.fn.CocActionAsync('doHover')
-    else
-        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
-    end
-end
-keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+-- Set up lspconfig.
+local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- LSP
 local lspconfig = require('lspconfig')
@@ -111,13 +171,62 @@ lspconfig.gopls.setup({
 			analyses = {
 				unusedparams = true,
 			},
-			staticcheck = true,
+--			staticcheck = true,
 			gofumpt = true,
-
 		}
 	},
+	-- NVIM-CMP + LSP
+	capabilities = cmp_capabilities,
 })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	desc = 'LSP actions',
+	callback = function()
+		local bufmap = function(mode, lhs, rhs)
+			local opts = { buffer = true }
+			vim.keymap.set(mode, lhs, rhs, opts)
+		end
+
+		-- Displays hover information about the symbol under the cursor
+		bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+
+		-- Jump to the definition
+		bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+
+		-- Jump to declaration
+		bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+
+		-- Lists all the implementations for the symbol under the cursor
+		bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+
+		-- Jumps to the definition of the type symbol
+		bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+
+		-- Lists all the references 
+		bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+
+		-- Displays a function's signature information
+		bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+
+		-- Renames all references to the symbol under the cursor
+		bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+
+		-- Selects a code action available at the current cursor position
+		bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+
+		-- Show diagnostics in a floating window
+		bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+
+		-- Move to the previous diagnostic
+		bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+
+		-- Move to the next diagnostic
+		bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+	end
+})
+
 
 -- General editor
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.guifont = "JetBrainsMono Nerd Font Mono:h14"
